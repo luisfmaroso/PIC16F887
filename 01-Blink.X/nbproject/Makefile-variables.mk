7fe33078b921e4de6e6b91230847aae14194e4bc
Luís Felipe Maroso <luisfmaroso@gmail.com>
2025-09-02 20:34:59 -0300

    remove build files and add them to gitignore

--- 01-Blink.X/nbproject/Makefile-variables.mk
+++ 01-Blink.X/nbproject/Makefile-variables.mk
@@ -6,5 +6,5 @@
 CND_BASEDIR=`pwd`
 # default configuration
 CND_ARTIFACT_DIR_default=dist/default/production
-CND_ARTIFACT_NAME_default=01-BLINK.X.production.hex
-CND_ARTIFACT_PATH_default=dist/default/production/01-BLINK.X.production.hex
+CND_ARTIFACT_NAME_default=01-Blink.X.production.hex
+CND_ARTIFACT_PATH_default=dist/default/production/01-Blink.X.production.hex
